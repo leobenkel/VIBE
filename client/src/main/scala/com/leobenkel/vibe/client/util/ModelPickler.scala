0374b9036a11c70ce49386ce0e0354976fe8bec9
Leo Benkel <lbenkel@demandbase.com>
2019-12-23 18:45:17 +0100

    Client side is getting closer to kinda working

--- client/src/main/scala/com/leobenkel/vibe/client/util/ModelPickler.scala
+++ client/src/main/scala/com/leobenkel/vibe/client/util/ModelPickler.scala
@@ -1,14 +1,18 @@
 package com.leobenkel.vibe.client.util
 
-import com.leobenkel.vibe.client.schemas.Temp
-import upickle.default.{macroRW, ReadWriter => RW, _}
+import com.leobenkel.vibe.core.Schemas._
+import upickle.default.{macroRW, ReadWriter => RW}
 
 /**
- * Here's where we define all of the model object's picklers and unpicklers.
- * You may want to move this to the shared project, though I like to keep them separately in case
- * you want to use a different method for marshalling json between the client and server
- */
+  * Here's where we define all of the model object's picklers and unpicklers.
+  * You may want to move this to the shared project, though I like to keep them separately in case
+  * you want to use a different method for marshalling json between the client and server
+  */
 object ModelPickler {
-  // TODO: To delete and import all the schema from 'core'
-  implicit val SampleModelObjectRW: RW[Temp] = macroRW
+  implicit val CommentRW:   RW[Comment] = macroRW
+  implicit val IdeaRW:      RW[Idea] = macroRW
+  implicit val SkillRW:     RW[Skill] = macroRW
+  implicit val TagRW:       RW[Tag] = macroRW
+  implicit val UserRW:      RW[User] = macroRW
+  implicit val UserVotesRW: RW[UserVotes] = macroRW
 }
